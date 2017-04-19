abebfcb15e818ce647a5e6ed44d388de174d5661
mojun <mojun@konka.com>
2017-04-19 18:01:51 +0800

    work save

--- script/cache.js
+++ script/cache.js
@@ -5,29 +5,60 @@ var key = "xxxxxx";
 window.onerror = function() {
 	return true;
 }
+// function doCache( folder, clsname, callback )
+function ajaxRequest(clsname, method, datas, callBack) {
+	var model = api.require('model');
+	var query = api.require("query");
 
-function ajaxRequest(url, method, datas, callBack) {
-	var serverUrl = serverurl;
-	var now = Date.now();
-	api.ajax({
-		url : serverUrl + url,
-		method : method,
-		cache : false,
-		timeout : 30,
-		dataType : 'json',
-		data : {
-			values : datas
-		}
-	}, function(ret, err) {
+	model.config({
+		appKey : '0D7660A8-5615-F246-E865-7CDD772B1653'
+	});
+	query.createQuery(function(ret, err) {
 		if (ret) {
-			callBack(ret, err);
-		} else {
-			api.alert({
-				msg : ('错误码：' + err.code + '；错误信息：' + err.msg + '网络状态码：' + err.statusCode)
+			var queryId = ret.qid;
+			query.limit({
+				qid : queryId,
+				value : "20"
+			});
+
+			model.findAll({
+				class : clsname,
+				qid : queryId
+			}, function(ret, err) {
+				if (ret) {
+					callBack(ret, err);
+				} else {					
+					console.log('错误码：' + err.code + '；错误信息：' + err.msg + '网络状态码：' + err.statusCode) ;					
+				}
 			});
 		}
+	});	
+}
+
+// 清除缓存，包括下载的文件、拍照临时文件、网页缓存文件等，清除时可能需要消耗一定时间。
+function clearCache() {
+	var cacheDir = api.cacheDir;
+	api.clearCache(function() {
+    api.toast({
+        msg: '清除完成'
+    });
+});
+}
+
+//删除文件
+function removefs(path) {
+	var fs = api.require('fs');
+	fs.remove({
+	    path: path
+	}, function(ret, err) {
+	    if (ret.status) {
+	        console.log(JSON.stringify(ret));
+	    } else {
+	        console.log(JSON.stringify(err));
+	    }
 	});
 }
+
 
 //读文件
 function readFile(path, callBack) {
@@ -40,12 +71,12 @@ function readFile(path, callBack) {
 }
 
 //写文件
-function writeFile(json, id, path) {
+function writeFile(json, path) {
 	//缓存目录
 	var cacheDir = api.cacheDir;
 	api.writeFile({
 		//保存路径
-		path : cacheDir + '/' + path + '/' + id + '.json',
+		path : cacheDir + path ,
 		//保存数据，记得转换格式
 		data : JSON.stringify(json)
 	}, function(ret, err) {
@@ -54,42 +85,48 @@ function writeFile(json, id, path) {
 }
 
 //缓存方法
-function doCache( folder, clsname, callback ) {
-	readFile('/' + folder + '/' + clsname + '.json', function(ret, err) {
+function doCache(folder, clsname, callback) {
+	var cachefile = '/' + folder + '/' + clsname + '.json' ;
+	console.log("doCache run");
+	readFile(cachefile, function(ret, err) {
 		if (ret.status) {
+			console.log('本地有缓存');
 			//如果成功，说明有本地存储，读取时转换下数据格式
 			//拼装json代码
 			//alert('取到缓存')
 			var cacheData = ret.data;
+			console.log( JSON.stringify(cacheData) );
+			//callback( JSON.parse( JSON.stringify(cacheData) ) );
 			callback(JSON.parse(cacheData));
-			iCache($('.cache'));
+			iCache($('.imgcache'));
 			//再远程取一下数据，防止有更新
-			ajaxRequest(url, 'GET', '', function(ret, err) {
+			ajaxRequest(clsname, 'GET', '', function(ret, err) {
 				if (ret) {
 					if (cacheData != JSON.stringify(ret)) {
 						//有更新处理返回数据
 						//alert('更新缓存')
 						callback(ret);
 						//缓存数据
-						writeFile(ret, id, folder);
+						writeFile(ret, cachefile);
 						iCache($('.cache'));
 					}
 				} else {
-					alert('数据获取失败！');
+					console.log('再远程取一下数据，防止有更新,数据获取失败！');
 				}
 			})
 		} else {
+			console.log('远程取数据');
 			//如果失败则从服务器读取，利用上面的那个ajaxRequest方法从服务器GET数据
-			ajaxRequest(url, 'GET', '', function(ret, err) {
+			ajaxRequest(clsname, 'GET', '', function(ret, err) {
 				if (ret) {
 					//处理返回数据
 					//alert('没取到缓存')
 					callback(ret);
 					//缓存数据
-					writeFile(ret, id, folder);
+					writeFile(ret, cachefile);
 					iCache($('.cache'));
 				} else {
-					alert('数据获取失败！');
+					console.log('远程取数据，数据获取失败！');
 				}
 			})
 		}
