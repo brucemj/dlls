77222a0ea4a59ffe1d54d90297a5fa3ec9af2dea
mojun <mojun@konka.com>
2017-05-18 18:08:06 +0800

    work save

--- script/public.js
+++ script/public.js
@@ -2,3 +2,20 @@
 function publog( strtext ){
 	console.log( strtext );
 }
+
+function openHead(headH){
+	var headH = arguments[0] ? arguments[0] : 51;
+	//var headH = headH||51; 
+    	api.openFrame ({
+            name: 'head',
+            url: 'widget://html/head.html',
+            bounces: false,
+            rect:{
+                x:0,
+                y: 0 ,
+                w:'auto',
+                h: headH
+            }
+        });
+    $api.setStorage("API_headHeight" , headH );
+}
